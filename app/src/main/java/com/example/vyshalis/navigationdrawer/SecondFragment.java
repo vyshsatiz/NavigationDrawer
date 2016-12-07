37aae8c7cdf1e531dcad4c44585f8f5259138e01
Vyshali <vyshalisatiz@gmail.com>
2016-12-07 21:44:04 +0530

    Initial commit

---
+++ app/src/main/java/com/example/vyshalis/navigationdrawer/SecondFragment.java
@@ -0,0 +1,24 @@
+package com.example.vyshalis.navigationdrawer;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Vyshali.S on 07-12-2016.
+ */
+
+public class SecondFragment extends Fragment {
+
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    myView = inflater.inflate(R.layout.second_layout, container, false);
+    return myView;
+}
+}
